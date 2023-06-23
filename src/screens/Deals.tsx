7ea3c8f1c3d9920619e1508df802a9735e319d9b
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-06-23 21:45:26 +0300

    Deals screen designed

--- src/screens/Deals.tsx
+++ src/screens/Deals.tsx
@@ -1,16 +1,65 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useEffect, useMemo } from 'react';
+import { RefreshControl, StyleSheet, View } from 'react-native';
 
+import { MasonryFlashList } from '@shopify/flash-list';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 
 import { BottomTabParamList } from '@/navigators/types';
+import MasonryProduct from '@/components/MasonryProduct';
+import { useAppDispatch, useAppSelector } from '@/store/store';
+import { fetchProducts } from '@/store/products';
+import useTheme from '@/hooks/useTheme';
+import { Spacing } from '@/theme';
+import { fetchHotDeals } from '@/store/hotDeals';
 
 type Props = NativeStackScreenProps<BottomTabParamList, 'Deals'>;
 
 const DealsScreen: React.FC<Props> = ({ navigation }) => {
+  const { colors } = useTheme();
+  const dispatch = useAppDispatch();
+  const { status: statusProduct, products } = useAppSelector(
+    store => store.products,
+  );
+  const { status: statusDeals, productIds } = useAppSelector(
+    store => store.hotDeals,
+  );
+
+  const getData = useCallback(() => {
+    dispatch(fetchProducts());
+    dispatch(fetchHotDeals());
+  }, []);
+
+  useEffect(() => getData(), []);
+
+  const isLoading = statusProduct === 'loading' || statusDeals === 'loading';
+
+  const dealProducts = useMemo(
+    () =>
+      products?.filter(product =>
+        productIds?.some(id => id.productId === product.id),
+      ) || [],
+    [products, productIds],
+  );
+
   return (
     <View style={styles.container}>
-      <Text>DealsScreen</Text>
+      <MasonryFlashList
+        data={dealProducts}
+        numColumns={2}
+        renderItem={({ item }) => <MasonryProduct product={item} />}
+        estimatedItemSize={200}
+        refreshControl={
+          <RefreshControl
+            refreshing={isLoading}
+            tintColor={colors.text}
+            colors={[colors.black]}
+            onRefresh={getData}
+          />
+        }
+        contentContainerStyle={{
+          padding: Spacing.extraSmall,
+        }}
+      />
     </View>
   );
 };
@@ -18,8 +67,6 @@ const DealsScreen: React.FC<Props> = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
 });
 
