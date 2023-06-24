7848e6ba776d7bf9e3f5ef08b19e88333eab8095
Kadir Aydınlı <kadiraydinli.4@gmail.com>
2023-06-24 11:10:09 +0300

    Basket screen list

--- src/screens/Basket.tsx
+++ src/screens/Basket.tsx
@@ -1,16 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
+import { FlashList } from '@shopify/flash-list';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 
 import { BottomTabParamList } from '@/navigators/types';
+import BasketProduct from '@/components/Basket/Product';
+import useBasket from '@/hooks/useBasket';
+import TotalPanel from '@/components/Basket/TotalPanel';
+import Header from '@/components/Basket/Header';
+import EmptyState from '@/components/EmptyState';
 
 type Props = NativeStackScreenProps<BottomTabParamList, 'Basket'>;
 
 const BasketScreen: React.FC<Props> = ({ navigation }) => {
+  const { products } = useBasket();
+
+  const emptyList = <EmptyState icon="shopping-bag" text="Empty Basket!" />;
+
+  if (!products?.length) {
+    return emptyList;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>BasketScreen</Text>
+      <Header />
+      <FlashList
+        data={products}
+        renderItem={({ item }) => <BasketProduct product={item} />}
+        estimatedItemSize={200}
+      />
+      <TotalPanel />
     </View>
   );
 };
@@ -18,8 +38,6 @@ const BasketScreen: React.FC<Props> = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
 });
 
